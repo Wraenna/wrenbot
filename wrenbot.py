c1327b56fafbb4ef400b02863a63fb25c615e043
Wren Bareilles <wren.ellbee@gmail.com>
2017-08-16 11:20:23 -0700

    Added some keywords. Case-insensitive, ignores punctuation.

--- wrenbot.py
+++ wrenbot.py
@@ -10,22 +10,45 @@ properly instead of the hack-y way using
 str(value).
 ----------------------------------------- """
 import discord
+import string
 
 client = discord.Client()
 
 @client.event
 async def on_message(message):
+
+    mentions_list = message.mentions
+    message_content = message.content.lower()
+    message_content = message_content.translate(message_content.maketrans("","", string.punctuation))
+    message_content = message_content.split()
     #Per the discord.py docs this is to not have the bot respond to itself
     if message.author == client.user:
         return
-    #Test command.
-    if message.content.startswith('!help'):
-        msg = '{0.author.mention}, this bot is designed to auto-reply to any mention of the Overwatch role.'.format(message)
-        await client.send_message(message.channel, msg)
     #Autoresponder for pinging Overwatch.
     for value in message.role_mentions:
         if str(value) == "Overwatch":
             msg = "Wrenbot says: Working so no."
+            await client.send_message(message.channel, msg)
+            return
+
+    #Autoresponder if Wren is pinged
+    for username in mentions_list:
+        if str(username) == "Synchronise#0768":
+            msg = "Pretend I made a sick pun here."
+            await client.send_message(message.channel, msg)
+            return
+
+    for word in message_content:
+        if word == "rando" or word == "randos":
+            msg = "To the other players, YOU are the rando!"
+            await client.send_message(message.channel, msg)
+            return
+        elif word == "gigantic":
+            msg = "See you on the airship!"
+            await client.send_message(message.channel, msg)
+            return
+        elif word == "rip":
+            msg = "rip in peperony and chease"
             await client.send_message(message.channel, msg)
             return
 
